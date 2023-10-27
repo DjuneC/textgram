c7f0aa835640edf5049d1b25a8730dbd49a951c0
DjuneC <cesarrodelin@outlook.com>
2023-10-27 13:21:39 -0500

    Distribute the layout between the respective components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 
 import SignInForm from "./_auth/forms/SignInForm"
 import SignUpForm from "./_auth/forms/SignUpForm"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
 import { Home } from "./_root/pages"
 
 const App = () => {
@@ -11,11 +13,16 @@ const App = () => {
     <main className="flex h-screen">
         <Routes>
             {/* Public routes */}
-            <Route path="/sign-in" element={ <SignInForm /> } />
-            <Route path="/sign-in" element={ <SignUpForm /> } />
-
+            <Route element={ <AuthLayout /> }>
+              <Route path="/sign-in" element={ <SignInForm /> } />
+              <Route path="/sign-up" element={ <SignUpForm /> } />
+            </Route>
+            
             {/* Private routes */}
-            <Route index element={ <Home /> }/>
+            <Route element={ <RootLayout/> }>
+              <Route index element={ <Home /> }/>
+            </Route>
+            
         </Routes>
     </main>
   )
