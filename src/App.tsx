f543e4ef7218684d90e00bfc543c1c4975094500
DjuneC <cesarrodelin@outlook.com>
2023-10-27 13:16:10 -0500

    Create Route and Form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { Routes, Route } from "react-router-dom"
 
 import "./globals.css"
 
+import SignInForm from "./_auth/forms/SignInForm"
+import SignUpForm from "./_auth/forms/SignUpForm"
+import { Home } from "./_root/pages"
+
 const App = () => {
   return (
     <main className="flex h-screen">
         <Routes>
             {/* Public routes */}
             <Route path="/sign-in" element={ <SignInForm /> } />
+            <Route path="/sign-in" element={ <SignUpForm /> } />
+
+            {/* Private routes */}
             <Route index element={ <Home /> }/>
         </Routes>
     </main>
